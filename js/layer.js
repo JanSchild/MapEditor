8353935671493d2ad9a66057080a5af0a5698aff
douyakasha <60048667+JanSchild@users.noreply.github.com>
2023-02-18 16:52:15 +0100

    made two methods private

--- js/layer.js
+++ js/layer.js
@@ -3,27 +3,27 @@ class Layer
     constructor(rows)
     {
         this.data = new Array();
-        this.expandToRows(rows);
+        this.#expandToRows(rows);
     }
 
-    expandToRows(rows)
+    #expandToRows(rows)
     {
         while(this.data.length < rows) this.data.push(new Array());
     }
 
-    row(y)
+    #row(y)
     {
         return this.data[y] ?? new Array();
     }
 
     tile(x, y)
     {
-        return this.row(y)[x] ?? Tile.emptyTile();
+        return this.#row(y)[x] ?? Tile.emptyTile();
     }
 
     setTile(tile, x, y)
     {
-        this.expandToRows(y + 1);
+        this.#expandToRows(y + 1);
         this.data[y][x] = tile;
     }
 
