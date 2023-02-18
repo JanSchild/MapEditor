79a927889acc101286963bdd9c32cbbe18089832
douyakasha <60048667+JanSchild@users.noreply.github.com>
2023-02-18 14:44:04 +0100

    added UI class (not yet functional)

---
+++ js/UI.js
@@ -0,0 +1,20 @@
+class UI
+{
+    static button = {
+        saveMap: ''
+    };
+
+    static textfield = {
+        mapName: '',
+        mapWidth: 20,
+        mapHeight: 15
+    };
+
+    static dropdown = {
+        tilesets: ''
+    }
+
+    static filechooser = {
+        mapUpload: ''
+    };
+}
