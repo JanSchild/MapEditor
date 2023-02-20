a0532fcc11bb387d8d9455af08250812b34bcc5b
douyakasha <60048667+JanSchild@users.noreply.github.com>
2023-02-20 17:18:14 +0100

    moved mapClick() from script.js to events.js

--- js/events.js
+++ js/events.js
@@ -31,6 +31,33 @@ UI.textfield.mapHeight.addEventListener('change', (event) =>
 UI.canvas.map.addEventListener('mousedown', mapClick);
 UI.canvas.map.addEventListener('mousemove', mapClick);
 
+function mapClick(event)
+{ 
+    // check if left mouse button is held down
+    if(event.buttons == 1)
+    {
+        var map_x = parseInt(event.offsetX / Tileset.tileWidth);
+        var map_y = parseInt(event.offsetY / Tileset.tileHeight);
+        var new_tile = new Tile(Tileset.current, Tileset.selectedX, Tileset.selectedY);
+        var old_tile = MapEditor.currentMap.layer.tile(map_x, map_y);
+
+        if(new_tile.isIdentical(old_tile)) return;
+
+        // flood tool 
+        if(activeKeys.has('KeyF'))
+        {
+            MapEditor.flood(map_x, map_y, new_tile);
+            return;
+        }
+        // single tile
+        MapEditor.setTile(new_tile, map_x, map_y);  
+
+        var changes = new TileChangeCollection();
+        changes.add(new TileChange(map_x, map_y, old_tile, new_tile));
+        History.add(changes);
+    }
+}
+
 // ACTIVE KEYS
 var activeKeys = new Set();
 window.addEventListener('keydown', (event) => { activeKeys.add(event.code) });
